a39dcf7087f286f294ab446d402c894174e13674
ThiliniM <madhurika.thilini@gmail.com>
2019-07-06 15:50:24 +0530

    Add compenet structure

---
+++ src/compenent/Home/Main/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Summary from './Summary';
+
+const Main = () => (<div>Main<Summary/></div>);
+
+export default Main;
---
+++ src/compenent/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+import Intro from './Intro';
+import Main from './Main';
+import Footer from './Footer';
+
+const Home = () => (<div>
+    <Header />
+    <Intro />
+    <Main />
+    <Footer />
+</div>);
+
+export default Home;
